a55e2dd06fc9c89205a3b1dc91dc19c98912256e
Bruno Macedo <brunoms@dcomp.ufs.br>
2018-07-19 00:12:34 +0000

    funcionando o aws

--- src/main/java/br/ufs/dcomp/ExemploRESTClientJava/RESTClient.java
+++ src/main/java/br/ufs/dcomp/ExemploRESTClientJava/RESTClient.java
@@ -13,19 +13,21 @@ public class RESTClient
             
             // JAVA 8 como pré-requisito (ver README.md)
             
-            String username = "user";
-            String password = "pass";
+            String username = "sender";
+            String password = "abc123";
      
             String usernameAndPassword = username + ":" + password;
             String authorizationHeaderName = "Authorization";
             String authorizationHeaderValue = "Basic " + java.util.Base64.getEncoder().encodeToString( usernameAndPassword.getBytes() );
      
             // Perform a request
-            String restResource = "https://cat.rmq.cloudamqp.com";
+            String restResource = "http://ec2-54-213-41-227.us-west-2.compute.amazonaws.com:15672";
             Client client = ClientBuilder.newClient();
             Response resposta = client.target( restResource )
             	//.path("/api/exchanges/iagffzqu/ufs/bindings/source") // lista todos os binds que tem "ufs" como source	
-                .path("/api/exchanges")
+                //.path("/api/exchanges")
+            	//.path("/api/queues")
+            	.path("/api/exchanges/%2f/bruno/bindings/source")
             	.request(MediaType.APPLICATION_JSON)
                 .header( authorizationHeaderName, authorizationHeaderValue ) // The basic authentication header goes here
                 .get();     // Perform a post with the form values
